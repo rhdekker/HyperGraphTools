b75fbf9ba229b2c14c1ff95e4c22affa1d5f38eb
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2019-02-12 10:23:41 +0100

    Add init to call commontokenstream

--- HRGParser/test/test.jl
+++ HRGParser/test/test.jl
@@ -4,9 +4,41 @@ test:
 - Author: bramb
 - Date: 2019-02-05
 =#
+import Pkg; Pkg.add("PyCall")
+
 using PyCall
 
 pushfirst!(PyVector(pyimport("sys")["path"]), @__DIR__) # add current directory to python path
+
+Pkg.add("Conda")
+using Conda
+
+Conda.add_channel("conda-forge")
 Conda.add("antlr-python-runtime")
 
 tagmllexer = pyimport("TAGMLLexer")
+
+function __init__()
+    py"""
+    from antlr4 import *
+    from TAGMLLexer import TAGMLLexer
+
+    def tokenize(filename):
+        file = open("test.tag", "r")
+        text = file.read()
+        file.close()
+        lexer = TAGMLLexer(InputStream(text))
+        return CommonTokenStream(lexer)
+    """
+end
+
+
+cs = py"tokenize"("test.tag")
+keys(cs)
+token0 = cs[:get](0)
+token1 = cs[:get](1)
+cs[:LT](-2)[:text]
+cs[:fill]()
+t=cs[:getTokens](0,25)
+map(x->x[:text],t)
+
